55d74389f444d9ead9f29a27a8411324d510c8f3
Euryomarbonilla23 <71295380+Euryomarbonilla23@users.noreply.github.com>
2021-03-14 00:04:27 -0400

    listow

--- obj/Debug/net5.0/RazorDeclaration/Pages/Index.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/Pages/Index.razor.g.cs
@@ -13,70 +13,70 @@ namespace ProyectoFinalBlazor.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\nallo\Documents\ProyectoFinalBlazor\_Imports.razor"
+#line 1 "c:\Users\nallo\Documents\ProyectoFinalBlazor\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\nallo\Documents\ProyectoFinalBlazor\_Imports.razor"
+#line 2 "c:\Users\nallo\Documents\ProyectoFinalBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\nallo\Documents\ProyectoFinalBlazor\_Imports.razor"
+#line 3 "c:\Users\nallo\Documents\ProyectoFinalBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\nallo\Documents\ProyectoFinalBlazor\_Imports.razor"
+#line 4 "c:\Users\nallo\Documents\ProyectoFinalBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\nallo\Documents\ProyectoFinalBlazor\_Imports.razor"
+#line 5 "c:\Users\nallo\Documents\ProyectoFinalBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\nallo\Documents\ProyectoFinalBlazor\_Imports.razor"
+#line 6 "c:\Users\nallo\Documents\ProyectoFinalBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\nallo\Documents\ProyectoFinalBlazor\_Imports.razor"
+#line 7 "c:\Users\nallo\Documents\ProyectoFinalBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\nallo\Documents\ProyectoFinalBlazor\_Imports.razor"
+#line 8 "c:\Users\nallo\Documents\ProyectoFinalBlazor\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\nallo\Documents\ProyectoFinalBlazor\_Imports.razor"
+#line 9 "c:\Users\nallo\Documents\ProyectoFinalBlazor\_Imports.razor"
 using ProyectoFinalBlazor;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\nallo\Documents\ProyectoFinalBlazor\_Imports.razor"
+#line 10 "c:\Users\nallo\Documents\ProyectoFinalBlazor\_Imports.razor"
 using ProyectoFinalBlazor.Shared;
 
 #line default
